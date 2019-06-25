bc73148f8110a8a6e58746de6b27357a4b7ee4d3
unknown <pedro.viegas.5s@icloud.com>
2019-06-25 00:23:28 -0300

    staticfiles_storage-settings.py

--- qrform/settings.py
+++ qrform/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE_CLASSES = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'qrform.urls'
 
